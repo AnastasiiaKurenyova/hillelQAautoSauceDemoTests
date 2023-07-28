84c9ee407a57a1ac7b4a7d5506127c4eb6a57d9e
AnastasiiaKurenyova <tristana360@gmail.com>
2023-07-28 13:05:23 +0300

    Added changes due to DriverSingleton changed

--- src/test/java/ua/ithillel/tests/BaseTest.java
+++ src/test/java/ua/ithillel/tests/BaseTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import ua.ithillel.driver.DriverSingleton;
-import ua.ithillel.driver.WebDriverType;
-import ua.ithillel.driver.factory.LocalWebDriverFactory;
 
 import java.time.Duration;
 
@@ -15,9 +13,7 @@ public class BaseTest {
 
     @BeforeEach
     public void browserSetup() {
-        LocalWebDriverFactory localWebDriverFactory = new LocalWebDriverFactory();
-        driver = localWebDriverFactory.getDriver(WebDriverType.CHROME);
-        DriverSingleton.setDriver(driver);
+        driver = DriverSingleton.getDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
